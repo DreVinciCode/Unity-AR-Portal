aa580248365f49ae642d30b191006c858ecbc69d
DreVinciCode <andre.cleaver@tufts.edu>
2022-06-11 09:36:00 -0400

    Removed build files

--- HololensBuild/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\AClea\Documents\GitHub\Unity-AR-Portal\HololensBuild\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\AClea\Documents\GitHub\Unity-AR-Portal\HololensBuild\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "C:\Users\AClea\Documents\GitHub\Unity-AR-Portal\HololensBuild\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
--- HololensBuild/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\AClea\Documents\GitHub\Unity-AR-Portal\HololensBuild\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
